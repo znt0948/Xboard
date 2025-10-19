6b37d9741648050b4e5933bf299b69e24c5a4c69
znt0948 <znt0948@users.noreply.github.com>
2025-10-19 16:57:55 +0800

    add device blacklist feature.

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\File;
-
+use App\Http\Controllers\V1\User\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
