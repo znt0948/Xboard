d9fc0e4ab0ac3c1dfecbcf241fc581d832a818e9
znt0948 <znt0948@users.noreply.github.com>
2025-10-22 15:39:00 +0800

    upgrade

--- app/Protocols/ClashMeta.php
+++ app/Protocols/ClashMeta.php
@@ -158,10 +158,10 @@ class ClashMeta extends AbstractProtocol
     public function buildRules($config)
     {
         // Force the current subscription domain to be a direct rule
-        $subsDomain = request()->header('Host');
-        if ($subsDomain) {
-            array_unshift($config['rules'], "DOMAIN,{$subsDomain},DIRECT");
-        }
+        //$subsDomain = request()->header('Host');
+        //if ($subsDomain) {
+        //    array_unshift($config['rules'], "DOMAIN,{$subsDomain},DIRECT");
+        //}
         // // Force the nodes ip to be a direct rule
         // collect($this->servers)->pluck('host')->map(function ($host) {
         //     $host = trim($host);
